a107108a356dc02d74ee5269a9e9a6c522a20a9a
jormao <jormao@gmail.com>
2019-11-19 17:51:23 -0500

    work in shell tuesday 19

--- prompt.c
+++ prompt.c
@@ -8,29 +8,35 @@ int main(void)
 	char *buffer = NULL, **array_words;
 	size_t buffer_size = 0;
     ssize_t prints = 1;
-	int i = 0;
+	int i = 0, words_to_free = 0;
 	 
 
         while (prints > 0)
         {
 			write(STDOUT_FILENO, "$ ", 2);
 			prints = getline(&buffer, &buffer_size, stdin);
+			if (prints == -1)
+				break;
+			words_to_free = words_in_string(buffer, " ");
 			array_words = split_string(buffer, " ");
 
-			while (array_words[i])
-			{
-				printf("SECTION MAIN | Value of prints: %s\n", array_words[i]);
-				i++;
-			}
-			i = 0;
+			printf("Before execve\n");
+    		if (execve(array_words[0], array_words, NULL) == -1)
+    		{
+        	perror("Error:");
+    		}
+    		printf("After execve\n");
 			
 			if (prints > 1)
 			{
 				write(STDOUT_FILENO, "$ ", 2);
 				write(STDOUT_FILENO, buffer, prints);
 			}
-        }
+			for (i = words_to_free; i >= 0; i--)
+				free(array_words[i]);
+			free(array_words);
+        }	
+
 	free(buffer);
-	free(array_words);
         return (0);
 }
