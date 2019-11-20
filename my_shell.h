cef74057418c2d3ede865249c463d19d28fc607d
jormao <jormao@gmail.com>
2019-11-20 15:24:12 -0500

    changes miercoles

--- my_shell.h
+++ my_shell.h
@@ -1,6 +1,13 @@
 #ifndef MY_SHELL_H
 #define MY_SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
 int words_in_string(char *str, char *delim);
 char **split_string(char *str, char *delim);
 #endif /* _MY_SHELL */
